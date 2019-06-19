9963a0a027f81a668a7219540c97ed77d96acab7
XfHua <jasonHuaxf@163.com>
2019-06-19 10:41:00 +0800

    Add files via upload

---
+++ App428/App428/App428.iOS/AppDelegate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+namespace App428.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            UIViewController1 control = new UIViewController1();
+
+            Window.RootViewController = control;
+            Window.MakeKeyAndVisible();
+
+            MessagingCenter.Subscribe<object, object>(this, "ShowMainScreen", (sender, args) =>
+            {
+                LoadApplication(new App());
+                base.FinishedLaunching(app, options);
+            });
+
+            return true;
+        }
+    }
+}
---
+++ App428/App428/App428.iOS/UIViewController1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+
+using CoreFoundation;
+using UIKit;
+using Foundation;
+using Xamarin.Forms;
+
+namespace App428.iOS
+{
+    [Register("UniversalView")]
+    public class UniversalView : UIView
+    {
+        public UniversalView()
+        {
+            Initialize();
+        }
+
+        public UniversalView(RectangleF bounds) : base(bounds)
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            BackgroundColor = UIColor.Red;
+        }
+    }
+
+    [Register("UIViewController1")]
+    public class UIViewController1 : UIViewController
+    {
+        public UIViewController1()
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            View = new UniversalView();
+
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view
+
+            int a = 0;
+
+            UILabel label = new UILabel();
+            label.Frame = new CoreGraphics.CGRect(100,150,100,50);
+            View.Add(label);
+            label.Font = UIFont.SystemFontOfSize(25);
+            label.Text = new NSString(a.ToString());
+
+
+            NSTimer.CreateScheduledTimer(1, true, (obj) =>
+            {
+                a++;
+                label.Text = new NSString(a.ToString());
+            });
+
+            //triggerd aft
+            NSTimer.CreateScheduledTimer(7, true, (obj) =>
+            {
+                MessagingCenter.Send<object, object>(this, "ShowMainScreen", null);
+            });
+        }
+    }
+}
